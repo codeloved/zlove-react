80d85c1914f83c395bde605e02dc52b7b477657d
zcc <1271821035@qq.com>
2019-10-25 00:16:31 +0800

    eslint

--- src/component/index.js
+++ src/component/index.js
@@ -1,5 +1,5 @@
 // UI组件
-export { default as BaseLayout } from './ui/layouts/baseLayout.js/index.js.js'
+export { default as BaseLayout } from './ui/layouts/baseLayout.js/index.js'
 export { default as LoginLayout } from './ui/layouts/loginLayout.js'
 
 // HOC组件
